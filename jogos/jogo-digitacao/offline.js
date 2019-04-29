8b9903c13ab516f3e021b97e88acb7ff5be49fb3
Ana Lumi <45796270+AnaLumi@users.noreply.github.com>
2019-04-29 16:18:55 -0300

    1st upload
    1st time uploading website

---
+++ jogos/jogo-digitacao/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1555087474,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/letras.png",
+		"images/palicedefault-sheet0.png",
+		"images/palicedefault-sheet1.png",
+		"images/palicedefault-sheet2.png",
+		"images/particles.png",
+		"images/bt_ok-sheet0.png",
+		"images/espaco_letras-sheet0.png",
+		"images/bt_apagar-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/leycherrytreehirez1600x-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
